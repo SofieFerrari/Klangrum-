e21323b3f983a9e86e0c6a96aa1e1e91b515d684
SofieFerrari <sofiestrahl@gmail.com>
2024-07-25 13:13:11 +0200

    installed dependencies for backend. Also worked on header and logo on hero image

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -9,9 +9,18 @@ export const Header = () => {
   return (
     <>
       <header className="relative w-full">
-        <img src={headerPic} className="h-auto w-full object-cover" />
-        <img src={logo} className="absolute left-[40%] top-[20%] w-24" />
-        <img src={hamburger} className="absolute right-2 top-2 w-5" />
+        <img src={headerPic} className="relative h-auto w-full object-cover" />
+        <img
+          src={logo}
+          className="absolute left-1/2 top-1/2 w-24 -translate-x-1/2 -translate-y-1/2 transform"
+        />
+        <select className="absolute right-2 top-2 w-5">
+          <option value="Architecture">Architechture & Design</option>
+          <option value="Illustrations">Illustrations</option>
+          <option value="Music">Music</option>
+          <option value="Events">Events</option>
+          <option value="About">About Me</option>
+        </select>
       </header>
     </>
   );

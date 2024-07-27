824306b34e63bf25dcd57112121a671caabd7e29
SofieFerrari <sofiestrahl@gmail.com>
2024-07-27 13:53:06 +0200

    added pictures and text to service section, need to scale down pics

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import profile from "../assets/images/fanny-profil.png";
 
 export const Contact = () => {
@@ -5,12 +6,14 @@ return (
   <>
     <section className="flex flex-col bg-yellow">
         <img src={profile} className="my-8 h-40 w-40 self-center rounded-full" />
+        {/* <Link to="/about"> */}
         <h1 className="text-center text-3xl mb-4">About me</h1>
+        {/* </Link> */}
         <div className="text-center text-xl leading-8 mb-5">
           <p>KLANG-RUM</p>
           <p>Fanny Skröder</p>
           <p>Porsmossevägen 1C 141 37 HUDDINGE</p>
-          <p>Phone: +46 70 155 04 60</p>
+          <p>+46 70 155 04 60</p>
           <p>Skroder.fanny@gmail.com</p>
         </div>
     </section>
--- frontend/src/components/Services.jsx
+++ frontend/src/components/Services.jsx
@@ -1,10 +1,21 @@
+import architerture from "../assets/images/fanny-ark.jpeg"
+import music from "../assets/images/fanny_event.jpeg";
 
 export const Services = () => {
   return (
-    <section className="bg-gray h-[150px]">
-      <div className="h-5">
-
+    <section className="relative flex h-auto flex-col bg-gray">
+      <div className="relative mt-5 w-11/12 self-center">
+        <img src={architerture} alt="Architecture" className="w-full" />
+        <h1 className="absolute inset-0 flex items-center justify-center text-3xl text-black">
+          Architecture
+        </h1>
+      </div>
+      <div className="relative mt-5 w-11/12 self-center">
+        <img src={music} alt="Music" className="w-full" />
+        <h1 className="absolute inset-0 flex items-center justify-center text-3xl text-black">
+          Music
+        </h1>
       </div>
     </section>
-  )
+  );
 }

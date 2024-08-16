ca13c949b7c5db7895574bb69fb28a054ab0a877
SofieFerrari <sofiestrahl@gmail.com>
2024-08-16 14:14:55 +0200

    changed the pxls back

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,7 +13,7 @@ export const Header = ({ Navbar }) => {
           src={logo}
           className="absolute left-1/2 bottom-8 w-24 -translate-x-1/2 -translate-y-1/2 transform md:left-4 md:top-4 md:w-24 md:translate-x-0 md:translate-y-0 lg:left-4 lg:top-4 lg:w-24"
         />
-        <div className="absolute p-2 bottom-1 m-1 whitespace-pre-line bg-white bg-opacity-60 text-left font-montserrat text-sm font-normal text-black md:m-12 md:bg-opacity-60 md:p-4 md:text-2xl md:font-light lg:bottom-20 lg:left-1/2 lg:top-1/2 lg:ml-2 lg:box-border lg:h-[200px] lg:w-[900px] lg:-translate-x-1/2 lg:-translate-y-1/2 lg:transform lg:pl-16 lg:text-5xl lg:font-light lg:leading-relaxed">
+        <div className="absolute p-2 bottom-1 m-1 whitespace-pre-line bg-white bg-opacity-60 text-left font-montserrat text-sm font-normal text-black md:m-12 md:bg-opacity-60 md:p-4 md:text-2xl md:font-light lg:bottom-20 lg:left-1/2 lg:top-1/2 lg:ml-2 lg:box-border lg:h-[200px] lg:w-[900px] lg:-translate-x-1/2 lg:-translate-y-1/2 lg:transform lg:pl-16 lg:text-5xl lg:font-normal lg:leading-relaxed">
           {`"I see music as fluid architecture"\n - Joni Mitchell`}
         </div>
       </header>

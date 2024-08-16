ca5ff8eab2a70e110cbba99ecb5803128460f686
SofieFerrari <sofiestrahl@gmail.com>
2024-08-16 11:14:44 +0200

    working on hamburger in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,7 +17,7 @@ export const Navbar = () => {
           <img src={hamburger} alt="navbar" />
         </button>
         {menuIsOpen && (
-          <nav className="flex self-center border-[20px] border-white">
+          <nav className="flex border-[20px] border-white">
             <ul className="list-none p-4">
               <li>
                 <a href="#architecture" className="block pb-6">

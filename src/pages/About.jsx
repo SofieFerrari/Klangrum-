fc9980c37321e488e7d36ebbe6f6c52224453afb
SofieFerrari <sofiestrahl@gmail.com>
2024-08-28 14:18:11 +0200

    added info to about page and arch page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,22 @@
 export const About = () => {
   return (
     <>
-    <h1>About section for Fanny</h1>
+      <div className="bg-gray flex flex-col p-8 font-montserrat">
+        <h1 className="text-3xl text-white font-bold tracking-widest text-right m-10">FANNY CHANDRA SKRÖDER</h1>
+        <p className="self-center tracking-wide text-right text-lg mx-10 mb-20 w-auto">
+          As an Interior Designer/Architect with 4 years of work experience, I
+          have mainly worked with store interiors and offices at the interior
+          design side. I have established a good ability to create selling rooms
+          and efficient flows - both for interior and exterior. In the role of
+          an Architect, I also have experience in drawing alterations and
+          additions with ROT orientation for private clients as well as building
+          permit documents for private clients and stores. And as a piano
+          player/teacher I've worked teaching kids and teenagers how to play
+          piano from the age of 6-25 years old. I've also worked as a barpiano
+          player at the Silja Line Galaxy. At last I've also played in several
+          pop/jazz/folk bands and been singing in different choirs.
+        </p>
+      </div>
     </>
-  )
+  );
 }
--- src/pages/Architecture.jsx
+++ src/pages/Architecture.jsx
@@ -1,7 +1,18 @@
 export const Architecture = () => {
   return (
     <>
-      <h1>Architecture and Design section</h1>
+      <div className="flex flex-col bg-gray p-8 font-montserrat">
+        <h1 className="m-10 text-right text-3xl font-bold tracking-widest text-white">
+          Architecture and Design
+        </h1>
+        <p className="mx-10 mb-20 w-auto self-center text-right text-lg tracking-wide">
+          Architecture Consultant Interior, Housing, Retail, Office, Workshops
+          As an architect I have mostly worked with building permits, office
+          planning, interior retail and recycling. I have also done workshops
+          for children. You can contact me if you want any help with building
+          permits for your house or interior design suggestions & solutions.
+        </p>
+      </div>
     </>
   );
 };

50fa30e3bdfd59f5a764b2188c614a64879c0913
SofieFerrari <sofiestrahl@gmail.com>
2024-08-30 12:45:17 +0200

    events.jsx is responsive, also added lines and p-element with text

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -7,7 +7,11 @@ export const Events = () => {
         <h1 className="m-10 text-right text-3xl font-bold tracking-widest text-white">
           Events
         </h1>
-        <ul className="mx-10 mb-20 w-auto self-center text-right tracking-wide">
+        <hr className="mb-10 border-t-2 border-white" />
+        <p className="mx-10 mb-3 w-auto text-left font-semibold tracking-wide">
+          Previous events
+        </p>
+        <ul className="mx-10 mb-10 w-auto self-center text-right tracking-wide">
           {eventData.map((event, index) => (
             <li className="mb-5 flex flex-col text-left" key={index}>
               <p className="text-xl font-bold">{event.name}</p>
@@ -16,6 +20,7 @@ export const Events = () => {
             </li>
           ))}
         </ul>
+        <hr className="mb-10 border-t-2 border-white" />
       </div>
     </>
   );

da9c081b8996e65c44412a58e457574b1b9ccd05
SofieFerrari <sofiestrahl@gmail.com>
2024-09-04 14:19:31 +0200

    added backtotop button

--- src/components/BackToTop.jsx
+++ src/components/BackToTop.jsx
@@ -1,12 +1,32 @@
-import { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 export const BackToTop = () => {
-  const { pathname } = useLocation();
+  const [isVisible, setIsVisible] = useState(false);
 
   useEffect(() => {
-    window.scrollTo(0, 0); 
-  }, [pathname]); 
+    const handleScroll = () => {
+      setIsVisible(window.scrollY > 300); // Show button if scrolled more than 300px
+    };
 
-  return null;
+    window.addEventListener("scroll", handleScroll);
+    // Clean up event listener on unmount
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
+  return (
+    isVisible && (
+      <button
+        onClick={scrollToTop}
+        className="fixed bottom-8 right-8 cursor-pointer whitespace-pre-line rounded-full border border-black bg-yellow p-4 text-xs text-black"
+        aria-label="Scroll to top"
+      >
+        {`Back\n to top`}
+      </button>
+    )
+  );
 };
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -6,12 +6,12 @@ import { Architecture } from "../pages/Architecture";
 import { Events } from "../pages/Events";
 import { Illustrations } from "../pages/Illustrations";
 import { Music } from "../pages/Music";
-// import { FirstPage } from "../pages/FirstPage"
+import { FirstPage } from "../pages/FirstPage"
 
 export const AppRoutes = () => {
   return (
     <Routes>
-      {/* <Route path="/" element={<FirstPage />} /> */}
+      <Route path="/" element={<FirstPage />} />
       <Route path="/home" element={<Homepage />} />
       <Route path="/architecture" element={<Architecture />} />
       <Route path="/illustrations" element={<Illustrations />} />

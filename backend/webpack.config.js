d9c6d8ca2e4636ed0d7719bdef593c079adf944f
SofieFerrari <sofiestrahl@gmail.com>
2024-09-13 12:03:36 +0200

    added webpack config file

---
+++ backend/webpack.config.js
@@ -0,0 +1,11 @@
+import path from "path";
+
+export default {
+  entry: "./src/server.js", // Ange ingångspunkten för din backend-kod
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  target: "node", // Viktigt att specificera att målet är Node.js
+  mode: "production", // Eller 'development', beroende på din miljö
+};

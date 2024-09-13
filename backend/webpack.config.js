7ed9d05ab6fff85288a776bf032af29856b61a6c
SofieFerrari <sofiestrahl@gmail.com>
2024-09-13 14:03:58 +0200

    idk

--- backend/webpack.config.js
+++ backend/webpack.config.js
@@ -1,11 +1,14 @@
-import path from "path";
+// backend/webpack.config.cjs
+const path = require("path");
+const nodeExternals = require("webpack-node-externals");
 
-export default {
-  entry: "./server.js", // Ange ingångspunkten för din backend-kod
+module.exports = {
+  entry: "./server.js", // Justera ingångspunkten om det behövs
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
-  target: "node", // Viktigt att specificera att målet är Node.js
-  mode: "production", // Eller 'development', beroende på din miljö
+  target: "node", // Viktigt att specificera Node.js
+  mode: "production", // Eller 'development'
+  externals: [nodeExternals()], // Exkludera Node.js-moduler från bundling
 };

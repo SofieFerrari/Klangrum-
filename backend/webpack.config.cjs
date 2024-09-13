84061ffcd168edcfca21819456580084acc1aadc
SofieFerrari <sofiestrahl@gmail.com>
2024-09-13 14:13:47 +0200

    idk3

--- backend/webpack.config.cjs
+++ backend/webpack.config.cjs
@@ -3,12 +3,12 @@ const path = require("path");
 const nodeExternals = require("webpack-node-externals");
 
 module.exports = {
-  entry: "./server.js", // Justera ingångspunkten om det behövs
+  entry: "./server.js", // Ange ingångspunkten för din backend-kod
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
-  target: "node", // Viktigt att specificera Node.js
-  mode: "production", // Eller 'development'
+  target: "node", // Viktigt att specificera att målet är Node.js
+  mode: "production", // Eller 'development', beroende på din miljö
   externals: [nodeExternals()], // Exkludera Node.js-moduler från bundling
 };
